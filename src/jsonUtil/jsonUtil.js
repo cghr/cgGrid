49628112767a120ea9a72379c223b96f000bc076
ravitej <ravitejvaranasi@gmail.com>
2015-02-24 11:55:31 +0530

    fix(jsonutil): change json util

--- src/jsonUtil/jsonUtil.js
+++ src/jsonUtil/jsonUtil.js
@@ -1,36 +1,35 @@
 angular.module('cgGrid.jsonUtil', ['cgGrid.lodash'])
-    .factory('JsonUtil', function (_) {
+  .factory('JsonUtil', function (_) {
 
-    	function getKeys(json){
-    		return _.keys(json);
-    	}
-    	function getHeaders(size,type){
+    function getKeys(json) {
+      return _.keys(json);
+    }
 
-    		return _.range(size)
-    		.map(function(){
-    			return type
-    		})
+    function getHeaders(size, type) {
 
-    	}
-    	
+      return _.range(size)
+        .map(function () {
+          return type
+        })
+
+    }
 
 
+    function transformJsonArray(jsonArray) {
 
-        function transformJsonArray(jsonArray) {
+      var gridData = _.map(jsonArray, function (obj, index) {
+        return {id: ++index, data: _.values(obj)}
+      });
 
-            var gridData = _.map(jsonArray, function (obj, index) {
-                return { id: ++index, data: _.values(obj) }
-            });
+      var keys = getKeys(jsonArray[0]);
+      var transformedData = {
+        headings: keys.join(','),
+        filters: getHeaders(keys.length, '#text_filter').join(','),
+        sortings: getHeaders(keys.length, 'str').join(','),
+        data: {rows: gridData}
+      };
+      return transformedData;
+    }
 
-            var keys=getKeys(jsonArray[0]);
-            var transformedData={
-            	headings:keys.join(','),
-            	filters:getHeaders(keys.length,'#text_filter').join(','),
-            	sortings:getHeaders(keys.length,'str').join(','),
-            	data:{rows:gridData}
-            };
-            return transformedData;
-        }
-
-        return { jsonToArray: transformJsonArray }
-    });
+    return {jsonToArray: transformJsonArray}
+  });
